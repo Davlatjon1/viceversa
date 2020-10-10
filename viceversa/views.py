67e695360b16bcddc91d911990382208849dfcde
Davlatjon99 <abdudavlat99@gmail.com>
2020-10-10 22:03:28 +0500

    edited views.py

--- viceversa/views.py
+++ viceversa/views.py
@@ -10,8 +10,9 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    count_word = len(user_text)
+    words = user_text.split()
+    number_of_words = len(words)
     print(reversed_text)
     return render(request, 'reverse.html', {'usertext': user_text,
                                             'reversedtext': reversed_text,
-                                            'count_word': count_word})
+                                            'count_word': number_of_words})
